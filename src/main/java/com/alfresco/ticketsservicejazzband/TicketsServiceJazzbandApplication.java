a76f41b0ccbf49e541dd84f3e24df4efbcedb37e
salaboy <salaboy@gmail.com>
2018-10-24 16:42:35 +0100

    Initial import

---
+++ src/main/java/com/alfresco/ticketsservicejazzband/TicketsServiceJazzbandApplication.java
@@ -0,0 +1,32 @@
+package com.alfresco.ticketsservicejazzband;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TicketsServiceJazzbandApplication {
+
+	@GetMapping("/tickets")
+	public Integer getAvailableTickets(){
+		return 1000;
+	}
+
+	@Configuration
+	public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests().anyRequest().permitAll()
+					.and().csrf().disable();
+		}
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketsServiceJazzbandApplication.class, args);
+	}
+}
